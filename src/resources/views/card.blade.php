8f52cfbd9e5123aca3de8a41b8400d92ff61b512
FedorDobry <fedya-dobrynin1@mail.ru>
2020-10-07 23:55:57 +0300

    DB

--- src/resources/views/card.blade.php
+++ src/resources/views/card.blade.php
@@ -4,12 +4,12 @@
         </div>
         <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
         <div class="caption">
-            <h3>iPhone X 64GB</h3>
-            <p>71990 ₽</p>
+            <h3>{{ $product->name }}</h3>
+            <p>{{ $product->price }} руб.</p>
             <p>
             <form action="{{ route('cart') }}" method="POST">
                 <button type="submit" class="btn btn-primary" role="button">В корзину</button>
-                <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"
+                <a href="{{ route('product', [$product->category->code, $product->code]) }}"
                    class="btn btn-default"
                    role="button">Подробнее</a>
                 <input type="hidden" name="_token" value="HbKaH2m3B1NLVwlKXkwYQ4ZDk7xEU5GuZnCbH8wX"></form>
